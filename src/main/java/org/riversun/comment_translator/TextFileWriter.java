64f09b64f0322144167611ac6960be1c4919b9b6
Tom Misawa <riversun.org@gmail.com>
2014-01-08 23:57:55 +0900

    Organize import

--- src/main/java/org/riversun/comment_translator/TextFileWriter.java
+++ src/main/java/org/riversun/comment_translator/TextFileWriter.java
@@ -19,7 +19,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
